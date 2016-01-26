739addb4416209dcb4af1a36abf1030f84050796
Rebecca Turner <me@re-becca.org>
2016-01-26 14:15:34 -0800

    Produce more consistently quoted, natural looking code

--- index.js
+++ index.js
@@ -78,13 +78,13 @@ var generateFromDir = Tacks.generateFromDir = function (dir, indent) {
     if (filename === '.git') return
     var filepath = path.join(dir, filename)
     var fileinfo = fs.statSync(filepath)
-    output += indent + '  ' + JSON.stringify(filename) + ': '
+    output += indent + '  ' + asObjectKey(filename) + ': '
     if (fileinfo.isDirectory()) {
       output += generateFromDir(filepath, indent + '  ')
     } else {
       var content = fs.readFileSync(filepath)
       output += 'File('
-      if (output.length == 0) {
+      if (output.length === 0) {
         output += "'')"
       } else {
         try {
@@ -106,16 +106,16 @@ var generateFromDir = Tacks.generateFromDir = function (dir, indent) {
 
 function outputAsJSON (indent, content) {
   var parsed = JSON.parse(content)
-  return JSON.stringify(parsed, null, 2).replace(/\n/g, '\n' + indent)
+  return asLiteral(parsed).replace(/\n/g, '\n' + indent)
 }
 
 function outputAsText (indent, content) {
   var endsInNewLine = /\n$/.test(content)
   var lines = content.toString('utf8').split(/\n/).map(function (line) { return line + '\n' })
   if (endsInNewLine) lines.pop()
-  var output = '\n' + indent + JSON.stringify(lines.shift())
+  var output = '\n' + indent + asLiteral(lines.shift())
   lines.forEach(function (line) {
-    output += ' +\n' + indent  + JSON.stringify(line)
+    output += ' +\n' + indent + asLiteral(line)
   })
   return output
 }
@@ -129,4 +129,69 @@ function outputAsBuffer (indent, content) {
   })
   output += ", 'hex')"
   return output
+}
+
+function asObjectKey (key) {
+  var isIdent = /^[a-zA-Z$_][a-zA-Z$_0-9]+$/.test(key)
+  return isIdent ? key : asLiteral(key)
+}
+
+function asLiteral (thing) {
+  if (thing === null) return 'null'
+  if (thing == null) return 'undefined'
+  if (typeof thing === 'boolean' || typeof thing === 'number') {
+    return thing.toString()
+  } else if (typeof thing === 'string') {
+    return asStringLiteral(thing)
+  } else if (thing instanceof Array) {
+    return asArrayLiteral(thing)
+  } else {
+    return asObjectLiteral(thing)
+  }
+}
+
+function asStringLiteral (thing) {
+  var str = thing.toString()
+    .replace(/\\/g, '\\\\')
+    .replace(/[\0]/g, '\\0')
+    .replace(/[\b]/g, '\\b')
+    .replace(/[\f]/g, '\\f')
+    .replace(/[\n]/g, '\\n')
+    .replace(/[\r]/g, '\\r')
+    .replace(/[\t]/g, '\\t')
+    .replace(/[\v]/g, '\\v')
+  if (/'/.test(str) && !/"/.test(str)) {
+    return '"' + str + '"'
+  } else {
+    return "'" + str.replace(/'/g, "\\'") + "'"
+  }
+}
+
+function asArrayLiteral (thing) {
+  if (!thing.length) return '[ ]'
+  var arr = '[\n'
+  function arrayItem (item) {
+    return asLiteral(item).replace(/\n(.*)(?=\n)/g, '\n  $1')
+  }
+  arr += arrayItem(thing.shift())
+  thing.forEach(function (item) {
+    arr += ',\n' + arrayItem(item)
+  })
+  arr += '\n]'
+  return arr
+}
+
+function asObjectLiteral (thing) {
+  var keys = Object.keys(thing)
+  if (!keys.length) return '{ }'
+  var obj = '{\n'
+  function objectValue (key) {
+    return asObjectKey(key) + ': ' + asLiteral(thing[key]).replace(/\n(.*)(?=\n)/g, '\n  $1')
+  }
+  obj += objectValue(keys.shift())
+  keys.forEach(function (key) {
+    obj += ',\n' + objectValue(key)
+  })
+  obj += '\n}'
+  return obj
 }
