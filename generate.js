85fe2c8f1d06a388828e4833bde9ed47d6904edc
Rebecca Turner <me@re-becca.org>
2016-02-03 14:20:46 -0800

    Start using yargs in the commandline tool

--- generate.js
+++ generate.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 'use strict'
+var argv = require('yargs')
+  .usage('Usage: $0 fixturedir')
+  .demand(1, 1)
+  .argv
 var Fixture = require('./index.js')
 
-var fixturedata = Fixture.generateFromDir(process.argv[2])
+var fixturedata = Fixture.generateFromDir(argv._[0])
 console.log(fixturedata)
